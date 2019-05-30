9b2506c0a795f18afc404e86549b672d299fbec9
Edward Song <edwardsong05@gmail.com>
2019-05-29 22:06:31 -0600

    refactored SignUpPage to create AccountForm component

---
+++ frontend/src/components/AccountForm.js
@@ -0,0 +1,196 @@
+import React from "react";
+import { connect } from "react-redux";
+import { SingleDatePicker } from "react-dates";
+import Select from "react-select";
+import moment from "moment";
+import API from "../utils/API";
+
+class AccountForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            calendarFocused: false,
+            date: moment(),
+            birthDate: moment(),
+            isLoaded: false,
+            interests: [],
+            error: undefined,
+            selectedInterests: []
+        };
+    }
+
+    onDateChange = (birthDate) => {
+        if (birthDate) {
+            this.setState(() => ({ birthDate }));
+        }
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
+    };
+
+    onInterestChange = (selectedOption) => {
+        this.setState({ selectedInterests: selectedOption });
+    };
+
+    componentDidMount() {
+        API.get("interests/").then(
+            (result) => {
+                const interests = result.data.map((interest) => {
+                    return { value: interest.interest_tag, label: interest.interest_tag };
+                });
+                this.setState({
+                    isLoaded: true,
+                    interests: interests
+                });
+            },
+
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        );
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit({
+            formInput: e.target,
+            interests: this.state.selectedInterests.map((interest) => {
+                return interest.value;
+            }),
+            date: this.state.date,
+            birthDate: this.state.birthDate
+        });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <p>Username*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.username &&
+                    this.props.auth.error.username.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="username" />
+                <p>150 characters of fewer. Letters, digits and @/./+/-/) only.</p>
+                <p>Email Address</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.email &&
+                    this.props.auth.error.email.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="email" />
+                <p>First Name*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.first_name &&
+                    this.props.auth.error.first_name.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="firstName" />
+                <p>Middle Name</p>
+                <input type="text" name="middleName" />
+                <p>Last Name*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.last_name &&
+                    this.props.auth.error.last_name.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="lastName" />
+                <p>Birth date</p>
+                <SingleDatePicker
+                    date={this.state.date}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={(day) => false}
+                />
+                <p>Country*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.country &&
+                    this.props.auth.error.country.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="country" />
+                <p>State*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.state &&
+                    this.props.auth.error.state.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="state" />
+                <p>Steet Name*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.street_name &&
+                    this.props.auth.error.street_name.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="streetName" />
+                <p>Postal Code*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.postal_code &&
+                    this.props.auth.error.postal_code.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="postalCode" />
+                <p>City*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.city &&
+                    this.props.auth.error.city.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="city" />
+                <p>Sex</p>
+                <select name="sex">
+                    <option>Male</option>
+                    <option>Female</option>
+                </select>
+                <p>Phone Number</p>
+                <input type="text" name="phoneNumber" />
+                <p>Interests*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.interests &&
+                    this.props.auth.error.interests.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <Select
+                    value={this.state.selectedInterests}
+                    onChange={this.onInterestChange}
+                    options={this.state.interests}
+                    isMulti
+                    name="interests"
+                />
+                <p>Password</p>
+                <p>Your password contain at least {this.state.minPasswordLength} characters.</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.password &&
+                    this.props.auth.error.password.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="password" />
+                <p>Password Confirmation*</p>
+                {!!this.props.auth.error &&
+                    !!this.props.auth.error.password &&
+                    this.props.auth.error.password.map((error) => {
+                        return <p key={error}>{error}</p>;
+                    })}
+                <input type="text" name="passwordConfirmation" />
+                <br />
+                <button>Sign Up</button>
+            </form>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth
+    };
+};
+
+export default connect(mapStateToProps)(AccountForm);
--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { SingleDatePicker } from "react-dates";
-import Select from "react-select";
+import { connect } from "react-redux";
+import AccountForm from "./AccountForm.js";
 import moment from "moment";
 import API from "../utils/API";
 import emailValidator from "email-validator";
+import { authFail } from "../actions/auth";
 
 class SignUpPage extends React.Component {
     constructor(props) {
@@ -19,27 +20,6 @@ class SignUpPage extends React.Component {
             selectedInterests: [],
             minPasswordLength: 8
         };
-    }
-
-    componentDidMount() {
-        API.get("interests/").then(
-            (result) => {
-                const interests = result.data.map((interest) => {
-                    return { value: interest.interest_tag, label: interest.interest_tag };
-                });
-                this.setState({
-                    isLoaded: true,
-                    interests: interests
-                });
-            },
-
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        );
     }
 
     onDateChange = (birthDate) => {
@@ -65,75 +45,74 @@ class SignUpPage extends React.Component {
         return true;
     };
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        const input = e.target;
+    onSubmit = (input) => {
         let error = {};
 
+        const formInput = input.formInput;
+
         // validate email
-        if (!emailValidator.validate(input.email.value)) {
+        if (!emailValidator.validate(formInput.email.value)) {
             error.email = ["Invalid email entered"];
         }
 
         // check password
-        if (input.password.value !== input.passwordConfirmation.value) {
+        if (formInput.password.value !== formInput.passwordConfirmation.value) {
             error.password = ["Passwords do not match"];
-        } else if (input.password.value.length < this.state.minPasswordLength) {
+        } else if (formInput.password.value.length < this.state.minPasswordLength) {
             error.password = ["Password does not meet minimum length"];
         }
 
         // check for blanks
-        if (input.username.value.length === 0) {
+        if (formInput.username.value.length === 0) {
             error.username = ["Field cannot be blank."];
         }
-        if (input.firstName.value.length === 0) {
+        if (formInput.firstName.value.length === 0) {
             error.first_name = ["Field cannot be blank."];
         }
-        if (input.lastName.value.length === 0) {
+        if (formInput.lastName.value.length === 0) {
             error.last_name = ["Field cannot be blank."];
         }
-        if (input.country.value.length === 0) {
+        if (formInput.country.value.length === 0) {
             error.country = ["Field cannot be blank."];
         }
-        if (input.state.value.length === 0) {
+        if (formInput.state.value.length === 0) {
             error.state = ["Field cannot be blank."];
         }
-        if (input.streetName.value.length === 0) {
+        if (formInput.streetName.value.length === 0) {
             error.street_name = ["Field cannot be blank."];
         }
-        if (input.postalCode.value.length === 0) {
+        if (formInput.postalCode.value.length === 0) {
             error.postal_code = ["Field cannot be blank."];
         }
-        if (input.city.value.length === 0) {
+        if (formInput.city.value.length === 0) {
             error.city = ["Field cannot be blank."];
         }
-        if (input.interests.value.length === 0) {
+        if (input.interests.length === 0) {
             error.interests = ["Field must contain atleast 1 selected choice"];
         }
 
         if (!this.isEmpty(error)) {
-            this.setState({ error: error });
+            console.log("client side error", error);
+            this.props.authFail(error);
             return;
         }
 
         const user = {
-            password: input.password.value,
-            username: input.username.value,
-            email: input.email.value,
-            first_name: input.firstName.value,
-            middle_name: input.middleName.value,
-            last_name: input.lastName.value,
-            birth_date: this.state.date.format("YYYY-MM-DD"),
-            country: input.country.value,
-            state: input.state.value,
-            street_name: input.streetName.value,
-            postal_code: input.postalCode.value,
-            city: input.city.value,
-            sex: input.sex.value === "Male" ? "M" : "F",
-            phone_number: input.phoneNumber.value,
-            interests: this.state.selectedInterests.map((interest) => {
-                return interest.value;
-            })
+            password: formInput.password.value,
+            username: formInput.username.value,
+            email: formInput.email.value,
+            first_name: formInput.firstName.value,
+            middle_name: formInput.middleName.value,
+            last_name: formInput.lastName.value,
+            birth_date: input.date.format("YYYY-MM-DD"),
+            country: formInput.country.value,
+            state: formInput.state.value,
+            street_name: formInput.streetName.value,
+            postal_code: formInput.postalCode.value,
+            city: formInput.city.value,
+            sex: formInput.sex.value === "Male" ? "M" : "F",
+            phone_number: formInput.phoneNumber.value,
+            interests: input.interests
         };
 
         API.post("accounts/", user)
@@ -142,131 +121,22 @@ class SignUpPage extends React.Component {
             })
             .catch((error) => {
                 console.log("error in account creation", error);
-                console.log(error.response.request.response);
-                this.setState({ error: error.response.request.response });
+                this.props.authFail(error.response.request.response);
             });
-
-        console.log(user);
     };
 
     render() {
-        return (
-            <form onSubmit={this.onSubmit}>
-                <p>Username*</p>
-                {!!this.state.error &&
-                    !!this.state.error.username &&
-                    this.state.error.username.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="username" />
-                <p>150 characters of fewer. Letters, digits and @/./+/-/) only.</p>
-                <p>Email Address</p>
-                {!!this.state.error &&
-                    !!this.state.error.email &&
-                    this.state.error.email.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="email" />
-                <p>First Name*</p>
-                {!!this.state.error &&
-                    !!this.state.error.first_name &&
-                    this.state.error.first_name.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="firstName" />
-                <p>Middle Name</p>
-                <input type="text" name="middleName" />
-                <p>Last Name*</p>
-                {!!this.state.error &&
-                    !!this.state.error.last_name &&
-                    this.state.error.last_name.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="lastName" />
-                <p>Birth date</p>
-                <SingleDatePicker
-                    date={this.state.date}
-                    onDateChange={this.onDateChange}
-                    focused={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    numberOfMonths={1}
-                    isOutsideRange={(day) => false}
-                />
-                <p>Country*</p>
-                {!!this.state.country &&
-                    !!this.state.error.country &&
-                    this.state.error.country.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="country" />
-                <p>State*</p>
-                {!!this.state.state &&
-                    !!this.state.error.state &&
-                    this.state.error.state.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="state" />
-                <p>Steet Name*</p>
-                {!!this.state.error &&
-                    !!this.state.error.street_name &&
-                    this.state.error.street_name.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="streetName" />
-                <p>Postal Code*</p>
-                {!!this.state.error &&
-                    !!this.state.error.postal_code &&
-                    this.state.error.postal_code.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="postalCode" />
-                <p>City*</p>
-                {!!this.state.error &&
-                    !!this.state.error.city &&
-                    this.state.error.city.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="city" />
-                <p>Sex</p>
-                <select name="sex">
-                    <option>Male</option>
-                    <option>Female</option>
-                </select>
-                <p>Phone Number</p>
-                <input type="text" name="phoneNumber" />
-                <p>Interests*</p>
-                {!!this.state.error &&
-                    !!this.state.error.interests &&
-                    this.state.error.interests.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <Select
-                    value={this.state.selectedInterests}
-                    onChange={this.onInterestChange}
-                    options={this.state.interests}
-                    isMulti
-                    name="interests"
-                />
-                <p>Password</p>
-                <p>Your password contain at least {this.state.minPasswordLength} characters.</p>
-                {!!this.state.error &&
-                    !!this.state.error.password &&
-                    this.state.error.password.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="password" />
-                <p>Password Confirmation*</p>
-                {!!this.state.error &&
-                    !!this.state.error.password &&
-                    this.state.error.password.map((error) => {
-                        return <p key={error}>{error}</p>;
-                    })}
-                <input type="text" name="passwordConfirmation" />
-                <br />
-                <button>Sign Up</button>
-            </form>
-        );
+        return <AccountForm onSubmit={this.onSubmit} />;
     }
 }
 
-export default SignUpPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        authFail: (error) => dispatch(authFail(error))
+    };
+};
+
+export default connect(
+    undefined,
+    mapDispatchToProps
+)(SignUpPage);
