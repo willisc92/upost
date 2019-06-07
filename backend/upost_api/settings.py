ec60ccea4ea6a793f9cb24a97ff6ce542d79f0b1
Edward Song <edwardsong05@gmail.com>
2019-06-06 19:15:02 -0600

    successful deployment with whitenoise

--- backend/upost_api/settings.py
+++ backend/upost_api/settings.py
@@ -165,12 +165,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontendapp', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontendapp', 'static', 'frontend')]
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/dist/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
 
-WHITENOISE_ROOT = os.path.join(BASE_DIR, 'frontendapp', 'static', 'frontend')
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'frontendapp', 'dist', 'root')
 
 
 # Media fields
