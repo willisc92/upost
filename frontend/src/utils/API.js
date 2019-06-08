6806c985621c3539bdd2f9f2143610b17f7d920e
Edward Song <edwardsong05@gmail.com>
2019-06-08 12:47:39 -0600

    finished merge with master

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    //baseURL: "http://127.0.0.1:8080/api/", // for production purposes
-    baseURL: "http://upost.mzwhk73mkb.us-west-2.elasticbeanstalk.com/api/", // for deployment purposes
+    baseURL: "http://127.0.0.1:8000/api/", // for production purposes (seperate django and react servers)
+    //baseURL: "http://127.0.0.1:8080/api/", // for test deployment purposes (waitress)
+    //baseURL: "http://upost.mzwhk73mkb.us-west-2.elasticbeanstalk.com/api/", // for deployment purposes (AWS)
     responseType: "json",
     headers: !!localStorage.getItem("token") ? { Authorization: `Token ${localStorage.getItem("token")}` } : null
 });
