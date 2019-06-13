d4d7f312ba19dd5643360ae9c3069389ef23aa75
Willis Cheung <willisc92@hotmail.com>
2019-06-12 20:34:23 -0600

    Forgot to add serializers and views for communities

---
+++ backend/upost/views/Communities.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from ..serializers.Communities import CommunitySerializer
+from ..models.Shared import Community
+
+
+class CommunityView(viewsets.ModelViewSet):
+    serializer_class = CommunitySerializer
+    queryset = Community.objects.all()
