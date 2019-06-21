4147866f8b159b53926ad2cabe9467d7bf3f9cf6
Willis Cheung <willis.cheung88@gmail.com>
2019-06-20 20:41:16 -0600

    Have recurring form working as intended, to implement with event and incentive forms.

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -18,8 +18,9 @@ import MyChannelDetail from "../components/pages/MyChannelDetail";
 import EditChannelPage from "../components/pages/EditChannelPage";
 import AddPostPage from "../components/pages/AddPostPage";
 import EditPostPage from "../components/pages/EditPostPage";
-import IncentiveForm from "../components/forms/IncentiveForm";
 import { setAuthToken } from "../utils/API";
+
+import { RecurringForm } from "../components/forms/RecurringForm";
 
 export const history = createHistory();
 
@@ -46,6 +47,7 @@ class AppRouter extends React.Component {
                         <PrivateRoute path="/myChannels/edit/:id" component={EditChannelPage} exact={true} />
                         <PrivateRoute path="/addChannel" component={AddChannelPage} exact={true} />
                         <PrivateRoute path="/interests" component={InterestsPage} exact={true} />
+                        <Route path="/RecurringForm" component={RecurringForm} exact={true} />
                         <Route component={NotFoundPage} />
                     </Switch>
                 </div>
