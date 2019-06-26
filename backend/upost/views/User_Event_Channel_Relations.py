581e8f66880821452a8b210f9e83f10e8f87a3f3
Edward Song <edwardsong05@gmail.com>
2019-06-25 21:38:44 -0600

    fixed initial creation of subscription

--- backend/upost/views/User_Event_Channel_Relations.py
+++ backend/upost/views/User_Event_Channel_Relations.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from ..serializers.User_Event_Channel_Relations import SubscribeSerializer
 from ..models.User_Event_Channel_Relations import Subscribe
+from ..models.Channels_Posts_Events import ContentChannel
+from ..models.User_Account import CustomUser
 from datetime import datetime
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,5 +25,7 @@ class SubscribeView(viewsets.ModelViewSet):
             subscribe.save()
             return Response(status=status.HTTP_200_OK)
         else:
-            subscribe = Subscribe.objects.create(request.data)
+            channel = ContentChannel.objects.get(pk=request.data['channel'])
+            user = CustomUser.objects.get(pk=request.data['community_member'])
+            subscribe = Subscribe.objects.create(channel=channel, community_member=user)
             return Response(status=status.HTTP_201_CREATED)
