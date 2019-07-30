ebc92067aeb6bdcab3f7b3a78b9c27e0ccae2409
Edward Song <edwardsong05@gmail.com>
2019-07-29 18:25:44 -0600

    changed incentive package to use chips

--- frontend/src/components/IncentivePackage.js
+++ frontend/src/components/IncentivePackage.js
@@ -1,19 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 import { startGetIncentiveTypes } from "../actions/incentive_types";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
+import InfoIcon from "@material-ui/icons/Info";
+import MessageModal from "../components/modals/MessageModal";
 
 class IncentivePackage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            openMessageModal: false
+        };
+    }
+
     componentDidMount() {
         if (!!this.props.incentives && this.props.incentives.length === 0) {
             this.props.startGetIncentiveTypes();
         }
     }
+
+    closeMessageModal = () => {
+        this.setState(() => {
+            return { openMessageModal: false };
+        });
+    };
+
+    showDietOptions = () => {
+        this.setState(() => {
+            return { openMessageModal: true };
+        });
+    };
 
     render() {
         const food = this.props.incentives.find((incentive) => {
@@ -21,51 +39,54 @@ class IncentivePackage extends React.Component {
         });
 
         return (
-            <Table>
-                <tbody>
-                    {this.props.package.incentive_type.includes("Food") && (
-                        <tr>
-                            <td>{!!food && <img src={food.incentive_image} />}</td>
-                            <td>{!!food && food.incentive_name}</td>
-                        </tr>
+            <div>
+                <div className="box-centered">
+                    {this.props.package.incentive_type.includes("Food") && !!food && (
+                        <Chip
+                            color="primary"
+                            label={food.incentive_name}
+                            style={{ margin: 5 }}
+                            avatar={<Avatar src={food.incentive_image} />}
+                            deleteIcon={<InfoIcon style={{ color: "white" }} />}
+                            clickable
+                            onDelete={this.showDietOptions}
+                            onClick={this.showDietOptions}
+                        />
                     )}
-                    {!!this.props.package.diet_option &&
-                        this.props.package.diet_option.map((option) => {
-                            return (
-                                <tr key={option}>
-                                    <td />
-                                    <td>{option}</td>
-                                </tr>
-                            );
-                        })}
-                </tbody>
-                <tbody>
                     {this.props.package.incentive_type.map((type) => {
-                        if (type === "Food") {
-                            return;
-                        } else {
+                        if (
+                            !!this.props.incentives.find((incentive) => {
+                                return incentive.incentive_name === type && incentive.incentive_name !== "Food";
+                            })
+                        ) {
                             return (
-                                <tr key={type}>
-                                    <td>
-                                        {!!this.props.incentives.find((incentive) => {
-                                            return incentive.incentive_name === type;
-                                        }) && (
-                                            <img
-                                                src={
-                                                    this.props.incentives.find((incentive) => {
-                                                        return incentive.incentive_name === type;
-                                                    }).incentive_image
-                                                }
-                                            />
-                                        )}
-                                    </td>
-                                    <td>{type}</td>
-                                </tr>
+                                <Chip
+                                    color="primary"
+                                    label={type}
+                                    style={{ margin: 5 }}
+                                    avatar={
+                                        <Avatar
+                                            src={
+                                                this.props.incentives.find((incentive) => {
+                                                    return incentive.incentive_name === type;
+                                                }).incentive_image
+                                            }
+                                        />
+                                    }
+                                    key={type}
+                                />
                             );
                         }
                     })}
-                </tbody>
-            </Table>
+                </div>
+                {!!this.props.package.diet_option && (
+                    <MessageModal
+                        isOpen={this.state.openMessageModal}
+                        closeMessageModal={this.closeMessageModal}
+                        message={this.props.package.diet_option.join(", ")}
+                    />
+                )}
+            </div>
         );
     }
 }
